db38297cb020222d35c3504e3c65dc075e58d0df
CARTech <carte@uvic.ca>
2012-03-06 20:04:33 -0800

    Remove cardToMovex

--- FreeBird/FreeBirdViewController.h
+++ FreeBird/FreeBirdViewController.h
@@ -18,10 +18,6 @@
 @interface FreeBirdViewController : UIViewController {
     Card *aCard;
     Card *cardToMove;
-    Card *cardToMove0;
-    Card *cardToMove1;
-    Card *cardToMove2;
-    Card *cardToMove3;
     NSMutableArray *columns;
     NSMutableArray *cards;
     NSMutableArray *freeCells;
@@ -47,10 +43,6 @@
 @property (nonatomic, retain) NSMutableArray *columns;
 @property (nonatomic, retain) NSMutableArray *cards;
 @property (nonatomic, retain) Card *cardToMove;
-@property (nonatomic, retain) Card *cardToMove0; 
-@property (nonatomic, retain) Card *cardToMove1; 
-@property (nonatomic, retain) Card *cardToMove2; 
-@property (nonatomic, retain) Card *cardToMove3; 
 @property (nonatomic, retain) UIImageView *deckNumberOne; 
 @property (nonatomic, retain) UIImageView *deckNumberTwo; 
 @property (nonatomic, retain) UIImageView *deckNumberThree; 
